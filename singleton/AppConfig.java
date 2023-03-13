19ada71163fd6ad18eaa28f73e2965750e368723
Sravan Kumar Dubasi <sravankumardubasi@gmail.com>
2023-03-13 19:58:25 +0530

    singleton

---
+++ singleton/AppConfig.java
@@ -0,0 +1,12 @@
+package Ooops.singleton;
+
+public class AppConfig {
+    private AppConfig(){
+
+    }
+    private static AppConfig obj=new AppConfig();
+    public static AppConfig getInstance(){
+     return obj;
+    }
+
+}
