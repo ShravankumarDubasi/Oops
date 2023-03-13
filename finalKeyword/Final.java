93b215214b38ce9c8d8cb8c61911f31939d894f0
Sravan Kumar Dubasi <sravankumardubasi@gmail.com>
2023-03-13 19:56:45 +0530

    finalKeyword

---
+++ finalKeyword/Final.java
@@ -0,0 +1,19 @@
+package finalKeyword;
+
+public class Final extends Student{
+
+
+
+
+    //      final int ROLL_NO=4;
+    public static void main(String[] args){
+        Student obj=new Student();
+        Student obj2=new Student();
+        obj=obj2;
+//        Final obj=new Final();
+
+
+
+
+    }
+}
---
+++ finalKeyword/Student.java
@@ -0,0 +1,18 @@
+package finalKeyword;
+
+public  class Student {
+
+    final String name;
+    final int roll;
+    {
+        roll=44;
+    }
+    public Student(){
+        name="sgsdg";
+
+    }
+    public final void descripton(){
+        System.out.println("Student name is"+name);
+    }
+
+}
